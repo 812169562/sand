372087e71d7d1c22281b169239f0ecf768fa1b2a
yy <1>
2018-01-12 23:11:42 +0800

    添加vue

---
+++ sand.admin/manager/manager/src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+import 'muse-ui/dist/theme-light.css'
+import {Container, Header, Aside, Main, Footer} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.config.productionTip = false
+Vue.use(MuseUI)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
---
+++ sand.admin/manager/manager/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/components/Index'
+import User from '@/components/users/User'
+import Tenant from '@/components/users/Tenant'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    { path: '/index',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/user',
+      name: 'User',
+      component: User
+    },
+    {
+      path: '/tenant',
+      name: 'Tenant',
+      component: Tenant
+    }
+  ]
+})
